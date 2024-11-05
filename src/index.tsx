eaa18061efa4d154fe9fc57e1c92c1561c35fa2a
Gabriela <gabibusuda123@gmail.com>
2024-11-04 21:36:35 -0300

    Searching api products

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import Cep from './Cep';
 
 const root = ReactDOM.createRoot(
@@ -8,9 +7,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <>
-      <App />
-      <Cep />
-    </>
+    <Cep />
   </React.StrictMode>
 );
